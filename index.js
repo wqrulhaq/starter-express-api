31afd28995e3dc0d505fc5d5230923e9b8ecc688
waqar <wqrulhaq@gmail.com>
2023-02-21 22:13:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
+
+const express = require('express');
+const app= express();
 app.listen(process.env.PORT || 3000)
+app.use(express.static('public'));
+app.use(express.json({limit: '100mb'}));
 app.post('/api', (request, response) => {
 	console.log(request.body);
 	response.json({
