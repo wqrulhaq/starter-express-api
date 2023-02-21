734e1bc332dfe1c14caf2ff0a12ac56707c6353d
waqar <wqrulhaq@gmail.com>
2023-02-21 11:02:01 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('Yoooo!')
 })
 app.listen(process.env.PORT || 3000)
 app.post('/api', (request, response) => {
