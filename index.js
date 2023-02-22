25298449bb9e039b587514a41425a96960a1a680
waqar <wqrulhaq@gmail.com>
2023-02-22 23:32:32 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 const express = require('express');
 const app= express();
+
+const CyclicDb = require("@cyclic.sh/dynamodb")
+const db = CyclicDb("sore-plum-beetle-tieCyclicDB")
+
+const animals = db.collection("animals")
 app.listen(process.env.PORT || 3000)
 app.use(express.static('public'));
 app.use(express.json({limit: '100mb'}));
@@ -11,4 +16,19 @@ app.post('/api', (request, response) => {
 		latitude: request.body.latitude,
 		longitude: request.body.longitude
 	});
+	// create an item in collection with key "leo"
+	let leo = await animals.set("leo", {
+	    type: "cat",
+	    color: "orange"
+	})
+
 });
+
+app.get('/api',(request, response) => {
+
+	// get an item at key "leo" from collection animals
+	let item = await animals.get("leo")
+	console.log(item)
+});
+
+
